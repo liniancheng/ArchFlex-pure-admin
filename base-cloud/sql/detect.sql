689191160c04ae9749ba990f4c254e8f6b001cc7
lihongbo-pei <1973742240@qq.com>
2025-08-25 23:12:21 +0800

    sql添加表的字段注释

--- base-cloud/sql/detect.sql
+++ base-cloud/sql/detect.sql
@@ -8,17 +8,17 @@ use arch-flex;
 -- ----------------------------
 DROP TABLE IF EXISTS `detect_img_records`;
 CREATE TABLE `detect_img_records`  (
-   `id` int NOT NULL AUTO_INCREMENT,
-   `input_img` varchar(255) NULL DEFAULT NULL,
-   `out_img` varchar(255) NULL DEFAULT NULL,
-   `confidence` varchar(255) NULL DEFAULT NULL,
-   `all_time` varchar(255) NULL DEFAULT NULL,
-   `conf` varchar(255) NULL DEFAULT NULL,
-   `weight` varchar(255) NULL DEFAULT NULL,
-   `username` varchar(255) NULL DEFAULT NULL,
-   `start_time` varchar(255) NULL DEFAULT NULL,
-   `label` varchar(255) NULL DEFAULT NULL,
-   `kind` varchar(255) NULL DEFAULT NULL,
+   `id` int NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+   `input_img` varchar(255) NULL DEFAULT NULL COMMENT '输入图片路径',
+   `out_img` varchar(255) NULL DEFAULT NULL COMMENT '输出图片路径',
+   `confidence` varchar(255) NULL DEFAULT NULL COMMENT '置信度',
+   `all_time` varchar(255) NULL DEFAULT NULL COMMENT '总耗时',
+   `conf` varchar(255) NULL DEFAULT NULL COMMENT '最小置信度阈值',
+   `weight` varchar(255) NULL DEFAULT NULL COMMENT '权重文件',
+   `username` varchar(255) NULL DEFAULT NULL COMMENT '用户名',
+   `start_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '开始时间',
+   `label` varchar(255) NULL DEFAULT NULL COMMENT '标签',
+   `kind` varchar(255) NULL DEFAULT NULL COMMENT '检测种类',
    `del_flag` int(11) COMMENT '是否删除 1-删除，0-未删除' DEFAULT '0',
    PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COMMENT='图片检测记录表';
@@ -28,14 +28,14 @@ CREATE TABLE `detect_img_records`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `detect_video_records`;
 CREATE TABLE `detect_video_records`  (
-     `id` int NOT NULL AUTO_INCREMENT,
-     `input_video` varchar(255) DEFAULT NULL,
-     `out_video` varchar(255) DEFAULT NULL,
-     `username` varchar(255) NULL DEFAULT NULL,
-     `start_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-     `conf` varchar(255) DEFAULT NULL,
-     `weight` varchar(255) DEFAULT NULL,
-     `kind` varchar(255) DEFAULT NULL,
+     `id` int NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+     `input_video` varchar(255) DEFAULT NULL COMMENT '输入视频路径',
+     `out_video` varchar(255) DEFAULT NULL COMMENT '输出视频路径',,
+     `username` varchar(255) NULL DEFAULT NULL COMMENT '用户名',
+     `start_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '开始时间',
+     `conf` varchar(255) DEFAULT NULL COMMENT '最小置信度阈值',
+     `weight` varchar(255) DEFAULT NULL COMMENT '权重文件',
+     `kind` varchar(255) DEFAULT NULL COMMENT '检测种类',
      `del_flag` int(11) COMMENT '是否删除 1-删除，0-未删除' DEFAULT '0',
      PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COMMENT='视频检测记录表';
@@ -45,13 +45,13 @@ CREATE TABLE `detect_video_records`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `detect_camera_records`;
 CREATE TABLE `detect_camera_records`  (
-      `id` int NOT NULL AUTO_INCREMENT,
-      `weight` varchar(255) DEFAULT NULL,
-      `conf` varchar(255) DEFAULT NULL,
-      `username` varchar(255) DEFAULT NULL,
-      `start_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-      `out_video` varchar(255) DEFAULT NULL,
-      `kind` varchar(255) DEFAULT NULL,
+      `id` int NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+      `weight` varchar(255) DEFAULT NULL COMMENT '权重文件',
+      `conf` varchar(255) DEFAULT NULL COMMENT '最小置信度阈值',
+      `username` varchar(255) DEFAULT NULL COMMENT '用户名',
+      `start_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '开始时间',,
+      `out_video` varchar(255) DEFAULT NULL COMMENT '输出视频路径',,
+      `kind` varchar(255) DEFAULT NULL COMMENT '检测种类',
       `del_flag` int(11) COMMENT '是否删除 1-删除，0-未删除' DEFAULT '0',
       PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COMMENT='摄像检测记录表';
