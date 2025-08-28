1f4e5b33dca94bd4c1111b9ca3276647a558760a
liniancheng <pan1103176380@outlook.com>
2025-08-28 21:35:38 +0800

    feat: 虫害专家页面

---
+++ web/src/views/detect/doctor/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div
+        class="chat-body__wrapper"
+        :class="['mt-4', !deviceDetection() && 'ml-16']"
+    >
+        <component :is="Bing" />
+    </div>
+</template>
+
+<script setup lang="ts">
+import { deviceDetection } from "@pureadmin/utils";
+import Bing from "./components/Bing.vue";
+</script>
+
+<style lang="scss" scoped>
+.chat-body__wrapper {
+    height: -webkit-fill-available;
+}
+</style>
